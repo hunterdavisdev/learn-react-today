2d49eb31353167b7773e639dc77cf3ea6a39d8bd
hunterdavisdev <hunterdavis.lua@gmail.com>
2020-07-28 17:24:56 -0500

    19 - Selected Recipe State and Recipe Edit Values

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
-import React, { useState, createContext, useEffect } from "react";
-import RecipeList from "./RecipeList";
-import RecipeEdit from "./RecipeEdit";
-import { v4 as uuidv4 } from "uuid";
-import "../css/App.css";
+import React, { useState, createContext, useEffect } from 'react';
+import RecipeList from './RecipeList';
+import RecipeEdit from './RecipeEdit';
+import { v4 as uuidv4 } from 'uuid';
+import '../css/App.css';
 
 export const RecipeContext = createContext();
-const LOCAL_STORAGE_KEY = "cookingWithReact.recipes";
+const LOCAL_STORAGE_KEY = 'cookingWithReact.recipes';
 
 function App() {
   const [recipes, setRecipes] = useState(sampleRecipes);
+  const [selectedRecipeId, setSelectedRecipeId] = useState();
+
+  const selectedRecipe = recipes.find((recipe) => recipe.id === selectedRecipeId);
+  console.log(selectedRecipe);
 
   /** Initial recipe load */
   useEffect(() => {
@@ -21,81 +25,78 @@ function App() {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(recipes));
   }, [recipes]);
 
-  const handleRecipeAdd = () => {
-    const newRecipe = {
-      id: uuidv4(),
-      name: "New Recipe",
-      servings: 1,
-      cookTime: "1:00",
-      instructions: "Instructions...",
-      ingredients: [
-        {
-          id: uuidv4(),
-          name: "Name",
-          amount: "1 Tbsp."
-        }
-      ]
-    };
-
-    setRecipes([...recipes, newRecipe]);
-  };
-
-  const handleRecipeDelete = id =>
-    setRecipes(recipes.filter(recipe => recipe.id !== id));
+  const handleRecipeAdd = () => setRecipes([...recipes, newRecipeTemplate]);
+  const handleRecipeDelete = (id) => setRecipes(recipes.filter((recipe) => recipe.id !== id));
+  const handleRecipeSelect = (id) => setSelectedRecipeId(id);
 
   const recipeContextValue = {
     handleRecipeAdd,
-    handleRecipeDelete
+    handleRecipeDelete,
+    handleRecipeSelect,
   };
 
   return (
     <RecipeContext.Provider value={recipeContextValue}>
       <RecipeList recipes={recipes} />
-      <RecipeEdit />
+      {selectedRecipe && <RecipeEdit recipe={selectedRecipe} />}
     </RecipeContext.Provider>
   );
 }
 
+const newRecipeTemplate = {
+  id: uuidv4(),
+  name: 'New Recipe',
+  servings: 1,
+  cookTime: '1:00',
+  instructions: 'Instructions...',
+  ingredients: [
+    {
+      id: uuidv4(),
+      name: 'Name',
+      amount: '1 Tbsp.',
+    },
+  ],
+};
+
 const sampleRecipes = [
   {
     id: uuidv4(),
-    name: "Plain Chicken",
+    name: 'Plain Chicken',
     servings: 3,
-    cookTime: "1:45",
-    instructions:
-      "1. Put salt on chicken.\n2. Put chicken in oven.\n3. Eat chicken.",
+    cookTime: '1:45',
+    instructions: '1. Put salt on chicken.\n2. Put chicken in oven.\n3. Eat chicken.',
     ingredients: [
       {
         id: uuidv4(),
-        name: "Chicken",
-        amount: "2 lbs."
+        name: 'Chicken',
+        amount: '2 lbs.',
       },
       {
         id: uuidv4(),
-        name: "Salt",
-        amount: "1 tbsp."
-      }
-    ]
+        name: 'Salt',
+        amount: '1 tbsp.',
+      },
+    ],
   },
   {
     id: uuidv4(),
-    name: "Plain Pork",
+    name: 'Plain Pork',
     servings: 5,
-    cookTime: "0:45",
-    instructions: "1. Put paprika on pork.\n2. Put pork in oven.\n3. Eat pork.",
+    cookTime: '0:45',
+    instructions: '1. Put paprika on pork.\n2. Put pork in oven.\n3. Eat pork.',
     ingredients: [
       {
         id: uuidv4(),
-        name: "Pork",
-        amount: "3 lbs."
+        name: 'Pork',
+        amount: '3 lbs.',
       },
       {
         id: uuidv4(),
-        name: "Paprika",
-        amount: "2 tbsp."
-      }
-    ]
-  }
+        name: 'Paprika',
+        amount: '2 tbsp.',
+      },
+    ],
+  },
 ];
 
 export default App;
--- src/components/RecipeEdit.js
+++ src/components/RecipeEdit.js
@@ -1,61 +1,49 @@
-import React from "react";
-import RecipeIngredientEdit from "./RecipeIngredientEdit";
+import React from 'react';
+import RecipeIngredientEdit from './RecipeIngredientEdit';
 
-export default function RecipeEdit() {
+export default function RecipeEdit({ recipe }) {
   return (
-    <div className="recipe-edit">
-      <div className="recipe-edit__remove-btn-container">
-        <button className="btn recipe-edit__remove-btn">&times;</button>
+    <div className='recipe-edit'>
+      <div className='recipe-edit__remove-btn-container'>
+        <button className='btn recipe-edit__remove-btn'>&times;</button>
       </div>
-      <div className="recipe-edit__details-grid">
-        <label className="recipe-edit__label" htmlFor="name">
+      <div className='recipe-edit__details-grid'>
+        <label className='recipe-edit__label' htmlFor='name'>
           Name
         </label>
-        <input
-          className="recipe-edit__input"
-          type="text"
-          name="name"
-          id="name"
-        />
-        <label className="recipe-edit__label" htmlFor="cookTime">
+        <input className='recipe-edit__input' type='text' name='name' id='name' value={recipe.name} />
+        <label className='recipe-edit__label' htmlFor='cookTime'>
           Cook Time
         </label>
-        <input
-          className="recipe-edit__input"
-          type="text"
-          name="cookTime"
-          id="cookTime"
-        />
-        <label className="recipe-edit__label" htmlFor="servings">
+        <input className='recipe-edit__input' type='text' name='cookTime' id='cookTime' value={recipe.cookTime} />
+        <label className='recipe-edit__label' htmlFor='servings'>
           Servings
         </label>
         <input
-          className="recipe-edit__input"
-          type="number"
+          className='recipe-edit__input'
+          type='number'
           min={1}
-          name="servings"
-          id="servings"
+          name='servings'
+          id='servings'
+          value={recipe.servings}
         />
-        <label className="recipe-edit__label" htmlFor="instructions">
+        <label className='recipe-edit__label' htmlFor='instructions'>
           Instructions
         </label>
-        <textarea
-          className="recipe-edit__input"
-          name="instructions"
-          id="instructions"
-        />
+        <textarea className='recipe-edit__input' name='instructions' id='instructions' value={recipe.instructions} />
       </div>
       <br />
-      <label className="recipe-edit__label">Ingredients</label>
-      <div className="recipe-edit__ingredient-grid">
+      <label className='recipe-edit__label'>Ingredients</label>
+      <div className='recipe-edit__ingredient-grid'>
         <div>Name</div>
         <div>Amount</div>
         <div></div>
-        <RecipeIngredientEdit />
-        <RecipeIngredientEdit />
+        {recipe.ingredients.map((ingredient) => (
+          <RecipeIngredientEdit key={ingredient.id} ingredient={ingredient} />
+        ))}
       </div>
-      <div className="recipe-edit__add-ingredient-btn-container">
-        <button className="btn btn--primary">Add Ingredient</button>
+      <div className='recipe-edit__add-ingredient-btn-container'>
+        <button className='btn btn--primary'>Add Ingredient</button>
       </div>
     </div>
   );
