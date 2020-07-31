531efb1a3151b91b7de3b908c8ff073c7550ddb3
Hunter Davis <developer.hunterdavis@gmail.com>
2020-07-31 17:07:00 -0500

    handleRecipeChange

--- src/components/RecipeEdit.js
+++ src/components/RecipeEdit.js
@@ -1,49 +1,84 @@
-import React from 'react';
-import RecipeIngredientEdit from './RecipeIngredientEdit';
+import React, { useContext } from "react";
+import RecipeIngredientEdit from "./RecipeIngredientEdit";
+import { RecipeContext } from "./App";
 
 export default function RecipeEdit({ recipe }) {
+  const { handleRecipeChange } = useContext(RecipeContext);
+
+  const handleChange = ({ target: { name, value } }) => {
+    if (name === "servings") {
+      handleRecipeChange(recipe.id, {
+        ...recipe,
+        [name]: parseInt(value) || ""
+      });
+    } else {
+      handleRecipeChange(recipe.id, { ...recipe, [name]: value });
+    }
+  };
+
   return (
-    <div className='recipe-edit'>
-      <div className='recipe-edit__remove-btn-container'>
-        <button className='btn recipe-edit__remove-btn'>&times;</button>
+    <div className="recipe-edit">
+      <div className="recipe-edit__remove-btn-container">
+        <button className="btn recipe-edit__remove-btn">&times;</button>
       </div>
-      <div className='recipe-edit__details-grid'>
-        <label className='recipe-edit__label' htmlFor='name'>
+      <div className="recipe-edit__details-grid">
+        <label className="recipe-edit__label" htmlFor="name">
           Name
         </label>
-        <input className='recipe-edit__input' type='text' name='name' id='name' value={recipe.name} />
-        <label className='recipe-edit__label' htmlFor='cookTime'>
+        <input
+          className="recipe-edit__input"
+          type="text"
+          name="name"
+          id="name"
+          value={recipe.name}
+          onChange={handleChange}
+        />
+        <label className="recipe-edit__label" htmlFor="cookTime">
           Cook Time
         </label>
-        <input className='recipe-edit__input' type='text' name='cookTime' id='cookTime' value={recipe.cookTime} />
-        <label className='recipe-edit__label' htmlFor='servings'>
+        <input
+          className="recipe-edit__input"
+          type="text"
+          name="cookTime"
+          id="cookTime"
+          value={recipe.cookTime}
+          onChange={handleChange}
+        />
+        <label className="recipe-edit__label" htmlFor="servings">
           Servings
         </label>
         <input
-          className='recipe-edit__input'
-          type='number'
+          className="recipe-edit__input"
+          type="number"
           min={1}
-          name='servings'
-          id='servings'
+          name="servings"
+          id="servings"
           value={recipe.servings}
+          onChange={handleChange}
         />
-        <label className='recipe-edit__label' htmlFor='instructions'>
+        <label className="recipe-edit__label" htmlFor="instructions">
           Instructions
         </label>
-        <textarea className='recipe-edit__input' name='instructions' id='instructions' value={recipe.instructions} />
+        <textarea
+          className="recipe-edit__input"
+          name="instructions"
+          id="instructions"
+          value={recipe.instructions}
+          onChange={handleChange}
+        />
       </div>
       <br />
-      <label className='recipe-edit__label'>Ingredients</label>
-      <div className='recipe-edit__ingredient-grid'>
+      <label className="recipe-edit__label">Ingredients</label>
+      <div className="recipe-edit__ingredient-grid">
         <div>Name</div>
         <div>Amount</div>
         <div></div>
-        {recipe.ingredients.map((ingredient) => (
+        {recipe.ingredients.map(ingredient => (
           <RecipeIngredientEdit key={ingredient.id} ingredient={ingredient} />
         ))}
       </div>
-      <div className='recipe-edit__add-ingredient-btn-container'>
-        <button className='btn btn--primary'>Add Ingredient</button>
+      <div className="recipe-edit__add-ingredient-btn-container">
+        <button className="btn btn--primary">Add Ingredient</button>
       </div>
     </div>
   );
